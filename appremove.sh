6d1aee982919a54bb637c3a2b6ff59eadd6f1bb6
Shiumano <75152752+shiumano@users.noreply.github.com>
2022-05-01 22:55:58 +0900

    Update appremove.sh
    ファイルを生成しないように変更
    繰り返しをいちいち書かないように変更

--- appremove.sh
+++ appremove.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-adb shell pm list package >> pkg.txt
-cat pkg.txt | sed -e "s/package://g" | grep 'docomo' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-cat pkg.txt | sed -e "s/package://g" | grep 'ntt' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-cat pkg.txt | sed -e "s/package://g" | grep 'auone' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-cat pkg.txt | sed -e "s/package://g" | grep 'rakuten' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-cat pkg.txt | sed -e "s/package://g" | grep 'kddi' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-cat pkg.txt | sed -e "s/package://g" | grep 'softbank' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
+pkgs=`adb shell pm list package |
+      sed -e "s/package://g"`  #メモリが吹き飛ぶほどアプリが入ってるなら訴訟モンだよ
+carrier_pkgs=`echo $pkg |
+              grep -e 'docomo'
+                   -e 'ntt'
+                   -e 'auone'
+                   -e 'rakuten'
+                   -e 'kddi'
+                   -e 'softbank'`
 
-cat rm.sh | sed -e "s/adb shell pm uninstall --user 0//g"
+echo $carrier_pkgs
 
 echo -n "以上のアプリが消去されます [Y/n]: "
 read ANS
 
 case $ANS in
   "" | [Yy]* )
-    sh rm.sh
-    rm rm.sh pkg.txt
+    echo $pkgs |\
+    while read pkg
+    do
+      adb shell pm uninstall --user 0 $pkg
+    done
     echo "アプリの消去を実行しました"
     ;;
   * )
-    rm rm.sh pkg.txt
     echo "処理を中止しました。"
     ;;
 esac
-
